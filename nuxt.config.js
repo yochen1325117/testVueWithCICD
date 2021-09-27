e35e82544fe5c8de345c7ca7753d286aeb819c36
YoChen0907 <yo.chen@104.com.tw>
2021-09-27 14:55:23 +0800

    add git run deploy

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,6 +2,7 @@ export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'nuxtProjectTest',
+    target: 'static',
     htmlAttrs: {
       lang: 'en'
     },
