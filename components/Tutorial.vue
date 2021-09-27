dd1a187a93612225fd81b21c97fcc5b1c9dfed82
YoChen0907 <yo.chen@104.com.tw>
2021-09-27 13:18:56 +0800

    try again

--- components/Tutorial.vue
+++ components/Tutorial.vue
@@ -8,7 +8,7 @@
       </a>
       <div class="mt-8 bg-white overflow-hidden shadow sm:rounded-lg p-6">
         <h2 class="text-2xl leading-7 font-semibold">
-          Welcome to your Nuxt Application
+          Welcome to your Nuxt Application try again
         </h2>
         <p class="mt-3 text-gray-600">
           Ahoy~Ahoy~ 
