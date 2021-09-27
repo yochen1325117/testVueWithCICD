089eb90f318423f2b8cf050f97b90903b2e52acd
YoChen0907 <yo.chen@104.com.tw>
2021-09-27 13:13:07 +0800

    add .travis.yml connect travic ci/cd test

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,7 +15,9 @@ export default {
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
   },
-
+  server: {
+    port: 8087, // default: 3000
+  },
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
   ],
