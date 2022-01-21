8f58eb36d0818f5b1a2f1b10d1c2bda66dc9784b
YoChen0907 <yo.chen@104.com.tw>
2022-01-21 09:56:05 +0800

    8ceb673

---
+++ _nuxt/static/1642730040/drawSomthing/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/drawSomthing", {data:[{}],fetch:{},mutations:[]});
---
+++ _nuxt/static/1642730040/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002FimageAnimation","\u002FModelCollectionArea","\u002FPickYourLoveAnimalToStore","\u002FdrawSomthing","\u002FcreateProjectNote","\u002FcomponentTestingLab","\u002FTestGetSomethingFromOutside"]})
